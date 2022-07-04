efa4b6d56a1327cca0e95f4da7fdf589f8a294bf
Sergio <sergio_rhr@hotmail.com>
2022-07-04 11:35:23 -0300

    mongodb session removed

--- server/app.ts
+++ server/app.ts
@@ -1,55 +1,64 @@
-import cors from 'cors';
-import express from 'express';
-import mongoose from "mongoose";
-import passport from 'passport';
-import session from 'express-session';
-import compression from 'compression';
+import cors from 'cors'
+import express from 'express'
+import mongoose from 'mongoose'
+import passport from 'passport'
+// import session from 'express-session'
+import compression from 'compression'
 
-import { ConfigUtil } from './utils';
-import { AuthRoute, MailRoute, TokenRoute } from "./routes";
+import { ConfigUtil } from './utils'
+import { AuthRoute, MailRoute, TokenRoute } from './routes'
 
-const Store = require('express-session').Store;
-const MongooseStore = require('mongoose-express-session')(Store);
+// const Store = require('express-session').Store
+// const MongooseStore = require('mongoose-express-session')(Store)
 
 class App {
-  public express: any;
+  public express: any
 
   constructor() {
-    this.express = express();
+    this.express = express()
 
-    this.setDatabase();
-    this.setConfiguration();
-    this.setRoutes();
+    this.setDatabase()
+    this.setConfiguration()
+    this.setRoutes()
 
-    this.getMemoryUsage();
+    this.getMemoryUsage()
   }
 
   getMemoryUsage() {
-    const used = process.memoryUsage();
+    const used = process.memoryUsage()
     for (let key in used) {
-      console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+      console.log(
+        `${key} ${Math.round((used[key] / 1024 / 1024) * 100) / 100} MB`
+      )
     }
   }
 
   setDatabase() {
     mongoose.connect(ConfigUtil.get('mongoose'))
     const databaseConnection = mongoose.connection
-    databaseConnection.on('error', console.error.bind(console, 'MongoDB Connection error'))
+    databaseConnection.on(
+      'error',
+      console.error.bind(console, 'MongoDB Connection error')
+    )
   }
 
   setConfiguration() {
-    this.express.use(express.json({ limit: "10mb" }))
-    this.express.use(cors({
-      credentials: true,
-      origin: ConfigUtil.get('cors')
-    }))
-    this.express.use(session({
-      secret: ConfigUtil.get('session.express'),
-      resave: false,
-      rolling: false,
-      saveUninitialized: true,
-      store: new MongooseStore({ mongoose }),
-    }));
+    this.express.use(express.json({ limit: '10mb' }))
+    this.express.use(
+      cors({
+        credentials: true,
+        origin: ConfigUtil.get('cors'),
+      })
+    )
+    // this.express.use(
+    //   session({
+    //     secret: ConfigUtil.get('session.express'),
+    //     resave: false,
+    //     rolling: false,
+    //     saveUninitialized: true,
+    //     store: new MongooseStore({ mongoose }),
+    //   })
+    // )
     this.express.use(compression())
     this.express.use(passport.session())
     this.express.use(express.urlencoded({ extended: true }))
